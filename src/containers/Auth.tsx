af9ce09d2acb711ae89bf8ba8a362651e0de68c7
Adam Damiano <onaimada@gmail.com>
2022-03-31 23:38:20 -0700

    Try redirecting directly to my channel

--- src/containers/Auth.tsx
+++ src/containers/Auth.tsx
@@ -33,7 +33,8 @@ class Auth extends Component<Props, State> {
       const idToken = await Twitch.verifyIdToken(tokens.id)
 
       if (tokens.redirect) {
-        this.props.history.replace(Page.Auth, { redirect: tokens.redirect })
+        // this.props.history.replace(Page.Auth, { redirect: tokens.redirect })
+        this.props.history.replace(Page.Auth, { redirect: '/AdamLearnsLive' })
       }
 
       this.props.setTokens(tokens.access, idToken)
